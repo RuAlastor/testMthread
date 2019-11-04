11468443d3f6da5ce97f70395f1b227a532fd9d7
RuAlastor <Skilzzzjeka@yandex.ru>
2019-11-04 13:42:27 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,196 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <queue>
+#include <pthread.h>
+#include <unistd.h>
+
+
+//------------------------------------------
+
+static const int THREAD_NUM  = 200;
+static const int BUFFER_SIZE = 200;
+static const int MSG_NUM     = 5;
+
+//------------------------------------------
+
+class Locker {
+
+public:
+    explicit Locker( pthread_mutex_t& mutexToLock ) : _mutex( mutexToLock ) {
+        pthread_mutex_lock( &_mutex );
+    }
+    ~Locker() {
+        pthread_mutex_unlock( &_mutex );
+    }
+
+private:
+    pthread_mutex_t& _mutex;
+
+};
+
+//------------------------------------------
+
+class MultithreadQueue {
+
+public:
+    MultithreadQueue() : _isFinished( false ) {
+        pthread_mutex_init( &_mutex, nullptr );
+        pthread_cond_init( &_cond, nullptr );
+
+        pthread_mutex_init( &_mutex_overflow, nullptr );
+        pthread_cond_init( &_cond_overflow, nullptr );
+    }
+    ~MultithreadQueue() {
+        _isFinished = true;
+
+        pthread_cond_broadcast( &_cond );
+        pthread_cond_broadcast( &_cond_overflow );
+
+        pthread_cond_destroy( &_cond );
+        pthread_mutex_destroy( &_mutex );
+
+        pthread_cond_destroy( &_cond_overflow );
+        pthread_mutex_destroy( &_mutex_overflow );
+    }
+
+    size_t size() { return _msg_queue.size(); }
+    void finish() {
+        _isFinished = true;
+
+        pthread_cond_broadcast( &_cond );
+        pthread_cond_broadcast( &_cond_overflow );
+    }
+
+    int addMsg( std::string inputMsg ) {
+        pthread_mutex_lock( &_mutex );
+
+        while ( _msg_queue.size() > BUFFER_SIZE ) {
+            pthread_mutex_unlock( &_mutex );
+            pthread_mutex_lock( &_mutex_overflow );
+
+            pthread_cond_wait( &_cond_overflow, &_mutex_overflow );
+
+            pthread_mutex_lock( &_mutex );
+            pthread_mutex_unlock( &_mutex_overflow );
+        }
+
+        _msg_queue.push( inputMsg );
+
+        pthread_cond_signal( &_cond );
+        pthread_mutex_unlock( &_mutex );
+        return 0;
+    }
+    std::string getMsg() {
+        pthread_mutex_lock( &_mutex );
+
+        std::string nextMsg;
+        if ( _msg_queue.size() == 0 ) {
+            while ( true ) {
+                if ( _isFinished ) {
+                    pthread_mutex_unlock( &_mutex );
+                    return "";
+                }
+                pthread_cond_wait( &_cond, &_mutex );
+                // While it was waking up, someone stole its msg
+                if ( _msg_queue.size() == 0 ) {
+                    continue;
+                }
+
+                // Everything's all right
+                pthread_mutex_lock( &_mutex_overflow );
+
+                nextMsg = _msg_queue.front();
+                _msg_queue.pop();
+
+                pthread_cond_signal( &_cond_overflow );
+                pthread_mutex_unlock( &_mutex_overflow );
+
+                pthread_mutex_unlock( &_mutex );
+                return nextMsg;
+            }
+        }
+        else {
+            //pthread_mutex_lock( &_mutex_overflow );
+
+            nextMsg = _msg_queue.front();
+            _msg_queue.pop();
+
+            pthread_cond_signal( &_cond_overflow );
+            pthread_mutex_unlock( &_mutex_overflow );
+
+            pthread_mutex_unlock( &_mutex );
+            return nextMsg;
+        }
+    }
+
+private:
+
+
+private:
+    bool            _isFinished;
+
+    pthread_mutex_t _mutex;
+    pthread_cond_t  _cond;
+
+    pthread_mutex_t _mutex_overflow;
+    pthread_cond_t  _cond_overflow;
+
+    std::queue< std::string > _msg_queue;
+
+};
+static MultithreadQueue mtQueue;
+
+//------------------------------------------
+
+void* printMsgs( void* rawData ) {
+    std::string threadNum = reinterpret_cast< const char* >( rawData );
+    std::string msg;
+    printf( "Thread %s started!\n", threadNum.c_str() );
+    //std::cout << "Thread " << threadNum << " started!\n";
+    while ( ( msg = mtQueue.getMsg() ) != "" ) {
+        //std::cout << threadNum << ": " << msg;
+        printf( "%s: %s;\n", threadNum.c_str(), msg.c_str() );
+    }
+
+    printf( "Thread %s stopped!\n", threadNum.c_str() );
+    //std::cout << "Thread " << threadNum << " stopped!\n";
+    pthread_exit( nullptr );
+}
+
+//------------------------------------------
+
+int main()
+{
+    pthread_t printingThreads[THREAD_NUM];
+    int i = 0;
+    for ( i = 0; i < THREAD_NUM; ++i ) {
+        pthread_create( &printingThreads[i], nullptr, printMsgs, reinterpret_cast< void* >( const_cast< char* >( std::to_string( i ).c_str() ) ) );
+        //usleep( 5000 );
+        //pthread_detach( printingThreads[i] );
+        //std::cout << "Thread " << i << " started!\n";
+    }
+
+    //sleep( 1 );
+
+    i = 0;
+    std::string msg = "Test: ";
+    while ( i < MSG_NUM ) {
+        mtQueue.addMsg( msg + std::to_string( i ) );
+        ++i;
+        //sleep( 1 );
+    }
+
+    mtQueue.finish();
+
+    void* tmp = nullptr;
+    for ( i = 0; i < THREAD_NUM; ++i ) {
+        pthread_join( printingThreads[i], &tmp );
+        printf( "Thread %d stopped in main!\n", i );
+        //std::cout << "Thread " << i << " stopped in main!\n";
+    }
+
+    std::cout << '\n' << '\n' << mtQueue.size() << '\n';
+
+    return 0;
+}
